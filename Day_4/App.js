c611048be7ba7e1856f23234d36b632b4ffa66b9
GANESH9403 <113353067+GANESH9403@users.noreply.github.com>
2023-10-04 17:35:24 +0530

    Add files via upload

---
+++ Day_4/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  
+
+  return (
+  <div className="box">
+  <div id='loader'></div>
+  </div>
+  );
+}
+
+export default App;
